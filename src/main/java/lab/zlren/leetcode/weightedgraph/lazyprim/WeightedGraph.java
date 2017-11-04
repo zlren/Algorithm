3c910852f6d87ecc69c6066e3b8a4a0e914dc00f
zlren <zlren2012@163.com>
2017-11-04 14:10:24 +0800

    LazyPrim 算法

---
+++ src/main/java/lab/zlren/leetcode/weightedgraph/lazyprim/WeightedGraph.java
@@ -0,0 +1,10 @@
+package lab.zlren.leetcode.weightedgraph.lazyprim;
+
+interface WeightedGraph<Weight extends Number & Comparable> {
+    public int V();
+    public int E();
+    public void addEdge(Edge<Weight> e);
+    boolean hasEdge(int v, int w);
+    void show();
+    public Iterable<Edge<Weight>> adj(int v);
+}
